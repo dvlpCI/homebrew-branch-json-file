ca7880dbc0309a5bbac78c2a380c32aab9093277
dvlproad <dvlproad@163.com>
2023-04-15 03:59:33 +0800

    【Fix】fix sha256

--- helloworld.rb
+++ helloworld.rb
@@ -2,7 +2,7 @@ class Helloworld < Formula
   desc "样例"
   homepage "https://github.com/dvlpCI/script-branch-json-file"
   url "https://github.com/dvlpCI/script-branch-json-file/archive/0.0.2.tar.gz"
-  sha256 "3fc183959f1733cd47278595c0d5ba5a9896b19f81c9ebd3e8fa3dc4c6b4e003"
+  sha256 "5c7a27c4bec2ec1d34b64943d3e5fbe7243d1f3848eb5e74d2685d94d6c2ef61"
 
   def install
     # Install script to bin
