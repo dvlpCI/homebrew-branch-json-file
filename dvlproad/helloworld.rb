60e6637a5078c417f1ae8433199134a80550c139
dvlproad <dvlproad@163.com>
2023-04-14 21:59:49 +0800

    【Feature】0.0.2

--- dvlproad/helloworld.rb
+++ dvlproad/helloworld.rb
@@ -1,7 +1,7 @@
 class DemoTool < Formula
   desc "样例"
   homepage "https://github.com/dvlpCI/homebrew-branch-json-file"
-  url "https://github.com/dvlpCI/homebrew-branch-json-file/archive/v0.0.1.tar.gz"
+  url "https://github.com/dvlpCI/homebrew-branch-json-file/archive/v0.0.2.tar.gz"
   sha256 "8143d7da9631f4d1122371832a5c3046cc3b73d1eb504e9cdc77d80a53637ab8"
 
   def install
--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+echo "hello world"
+
+echo "hello world"
+
+echo "hello world"
+
+echo "tar -czvf v0.0.2.tar.gz helloworld.sh"
+
+echo "mv v0.0.2.tar.gz archive/"
+
+echo "hello world"
+
+echo "hello world"
+
+echo "hello world"
+
+echo "hello world"
+
 echo "hello world"
